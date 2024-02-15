84c9f534867a30cd68c1973c3f574afcb33c0683
Abhishek kumar <abhi.arya97161@gmail.com>
2024-02-16 00:46:08 +0530

    view components created

--- todoapp/src/app/app.routes.ts
+++ todoapp/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-// import { IndexComponent } from './post/index/index.component';
-// import { CreateComponent } from './post/create/create.component';
-// import { EditComponent } from './post/edit/edit.component';
-// import { ViewComponent } from './post/view/view.component';
+import { IndexComponent } from './post/index/index.component';
+import { CreateComponent } from './post/create/create.component';
+import { EditComponent } from './post/edit/edit.component';
+import { ViewComponent } from './post/view/view.component';
 
 export const routes: Routes = [
   {path:'', redirectTo:'post/index', pathMatch:'full'},
-//   {path:'post/index', component:IndexComponent},
-//   {path:'post/create', component:CreateComponent},
-//   {path:'post/:postId/edit', component:EditComponent},
-//   {path:'post/:postId/view', component:ViewComponent}
+  {path:'post/index', component:IndexComponent},
+  {path:'post/create', component:CreateComponent},
+  {path:'post/:postId/edit', component:EditComponent},
+  {path:'post/:postId/view', component:ViewComponent}
 ];  
