7a58068c2ace72f62b7896d873b35cdfff742383
Abhishek kumar <abhi.arya97161@gmail.com>
2024-02-16 00:04:55 +0530

    index.html updated

--- todoapp/src/app/app.routes.ts
+++ todoapp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TodolistComponent } from './components/todo/todolist/todolist.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { SignupComponent } from './components/user/signup/signup.component';
+import { NetworkerrorComponent } from './components/others/networkerror/networkerror.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'/login',pathMatch:'full'},
+    {path:'**', component:LoginComponent},
+    {path:'todo', component:TodolistComponent},
+    {path:'login', component:LoginComponent},
+    {path:'signup', component:SignupComponent},
+    {path:'network-error', component:NetworkerrorComponent},
+];
